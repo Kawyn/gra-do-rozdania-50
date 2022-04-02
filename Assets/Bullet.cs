cd68b66ff1c48f5d7c585432b7321b89f8a2cbca
Kawyn <kewinignasiak.2001@gmail.com>
2022-04-02 22:22:09 +0200

    Dziewczyna - Bolesław Leśmian

--- Assets/Bullet.cs
+++ Assets/Bullet.cs
@@ -1,17 +1,31 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
-    public Vector2 velocity;
-    
+    public int damage = 10;
+
+    [HideInInspector] public Vector2 velocity;
+
+    [SerializeField] private LayerMask mask;
+
     private void Update()
     {
-        RaycastHit2D hit = Physics2D.Raycast(transform.position, velocity.normalized, velocity.magnitude);
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, velocity.normalized, velocity.magnitude * Time.deltaTime, mask);
 
         transform.Translate(velocity * Time.deltaTime);
+
         if (hit)
-            Debug.Log(hit);
+        {
+            GameObject target = hit.transform.gameObject;
+
+            if (target.CompareTag("Wall"))
+            {
+                throw new System.NotImplementedException();
+            }
+            else
+            {
+                Destroy(gameObject);  
+            }
+        }
     }
 }
